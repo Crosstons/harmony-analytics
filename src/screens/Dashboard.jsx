c81ca5676557f4b189f2365cbfca2f3bef81535b
Crostons <shubh1608003@gmail.com>
2022-09-22 15:01:02 +0530

    Dashboard changes

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import logo from '../images/logo.png';
 function Dashboard() {
   return (
     <div>
@@ -7,23 +7,100 @@ function Dashboard() {
         <header class="text-gray-600 body-font">
   <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
     <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="500" zoomAndPan="magnify" viewBox="0 0 375 374.999991" height="500" preserveAspectRatio="xMidYMid meet" version="1.0"><defs><g/><clipPath id="b91791eaf7"><path d="M 46.59375 107 L 328.023438 107 L 328.023438 348.292969 L 46.59375 348.292969 Z M 46.59375 107 " clip-rule="nonzero"/></clipPath><clipPath id="4b689c84ef"><path d="M 46.59375 27.152344 L 327 27.152344 L 327 136 L 46.59375 136 Z M 46.59375 27.152344 " clip-rule="nonzero"/></clipPath></defs><g clip-path="url(#b91791eaf7)"><path fill="#28b78d" d="M 326.210938 107.382812 L 328.117188 267.292969 L 194.558594 346.800781 C 191.289062 348.746094 187.25 348.789062 183.945312 346.929688 L 48.511719 270.660156 L 46.59375 110.761719 L 91.316406 135.949219 L 91.324219 135.949219 L 92.621094 244.398438 L 183.332031 295.488281 C 186.636719 297.347656 190.679688 297.292969 193.9375 295.359375 L 283.398438 242.105469 L 282.082031 132.636719 L 282.964844 133.125 L 326.210938 107.382812 " fill-opacity="1" fill-rule="nonzero"/></g><g clip-path="url(#4b689c84ef)"><path fill="#f7f7f7" d="M 46.59375 110.761719 L 180.035156 29.355469 C 183.351562 27.332031 187.492188 27.285156 190.851562 29.21875 L 326.210938 107.402344 L 282.101562 133.65625 L 191.507812 80.734375 C 188.128906 78.753906 183.945312 78.808594 180.613281 80.859375 L 91.324219 135.949219 L 46.59375 110.761719 " fill-opacity="1" fill-rule="nonzero"/></g><g fill="#28b78d" fill-opacity="1"><g transform="translate(135.900951, 236.361773)"><g><path d="M 70.234375 -99.320312 L 70.234375 -61.011719 L 32.777344 -61.011719 L 32.777344 -99.320312 L 11.351562 -99.320312 L 11.351562 0 L 32.777344 0 L 32.777344 -41.003906 L 70.234375 -41.003906 L 70.234375 0 L 91.660156 0 L 91.660156 -99.320312 Z M 70.234375 -99.320312 "/></g></g></g></svg>
-
-      <span class="ml-3 text-xl">Tailblocks</span>
+    <img className="w-10 h-10" src={logo} />   
+      <span class="ml-3 font-barlow text-2xl text-mild font-thin">Harmony Analytics</span>
     </a>
     <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-      <a class="mr-5 hover:text-gray-900">First Link</a>
-      <a class="mr-5 hover:text-gray-900">Second Link</a>
-      <a class="mr-5 hover:text-gray-900">Third Link</a>
-      <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+     
     </nav>
-    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
-      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-        <path d="M5 12h14M12 5l7 7-7 7"></path>
-      </svg>
-    </button>
+    <button class=" text-mild bg-gradient-to-r from-emerald-400 via-emerald-500 to-emerald-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-emerald-800 shadow-lg dark:shadow-lg shadow-emerald-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-1">Wallet</button>
   </div>
 </header>
+
+//Transaction Datatable
+
+
+<div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <caption class="p-5 text-lg font-semibold text-left text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+            Our products
+            <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">Browse a list of Flowbite products designed to help you work and play, stay organized, get answers, keep in touch, grow your business, and more.</p>
+        </caption>
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6">
+                    Product name
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Color
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Category
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Price
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    <span class="sr-only">Edit</span>
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="py-4 px-6">
+                    Sliver
+                </td>
+                <td class="py-4 px-6">
+                    Laptop
+                </td>
+                <td class="py-4 px-6">
+                    $2999
+                </td>
+                <td class="py-4 px-6 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Microsoft Surface Pro
+                </th>
+                <td class="py-4 px-6">
+                    White
+                </td>
+                <td class="py-4 px-6">
+                    Laptop PC
+                </td>
+                <td class="py-4 px-6">
+                    $1999
+                </td>
+                <td class="py-4 px-6 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    Magic Mouse 2
+                </th>
+                <td class="py-4 px-6">
+                    Black
+                </td>
+                <td class="py-4 px-6">
+                    Accessories
+                </td>
+                <td class="py-4 px-6">
+                    $99
+                </td>
+                <td class="py-4 px-6 text-right">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
 
     </div>
   )
