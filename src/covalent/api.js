1121a487b635c293f663794f916a1bc93edd3c3f
Vraj Desai <68881630+vking45@users.noreply.github.com>
2022-09-22 15:46:48 +0530

    portfolio value added

--- src/covalent/api.js
+++ src/covalent/api.js
@@ -3,9 +3,9 @@ const APIKEY = 'ckey_f2a7d8e02ddb427ea2764a67733';
 const baseURL = 'https://api.covalenthq.com/v1';
 const blockchainChainId = '1666600000';
 
-export const getWalletBalance = async(address) => {
+export const getHRC20Balances = async(address) => {
     const res = await axios.get(`${baseURL}/${blockchainChainId}/address/${address}/balances_v2/?key=${APIKEY}`);
-    return res.data.data;
+    return res.data.data.items;
 }
 
 export const getTransactions = async(address, token) => {
