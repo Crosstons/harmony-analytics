594b0c474d7f33ce674f25bf9f812f99e5197b59
Crostons <shubh1608003@gmail.com>
2022-09-22 23:23:09 +0530

    Grids added

--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,11 +1,137 @@
 import React from 'react';
+import Lchart from '../components/Lchart';
+
 function Dashboard() {
   return (
     <div class="grid overflow-hidden lg grid-cols-4 grid-rows-8 gap-2 w-screen h-screen">
-	<div class="box row-start-1 row-end-1 col-start-1 col-end-5">1</div>
-	<div class="box row-start-2 row-span-4 col-start-1 col-span-3">2</div>
-	<div class="box row-start-2 row-span-2 col-start-4 col-end-5">3</div>
-	<div class="box row-start-4 row-span-2 col-end-5">4</div>
+	<div class="box row-start-1 row-end-1 col-start-1 col-end-5 text-spicy">
+    <header class="text-gray-600 body-font">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+    <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+      </svg>
+      <span class="ml-3 text-xl">Tailblocks</span>
+    </a>
+    <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+      <a class="mr-5 hover:text-gray-900">First Link</a>
+      <a class="mr-5 hover:text-gray-900">Second Link</a>
+      <a class="mr-5 hover:text-gray-900">Third Link</a>
+      <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+    </nav>
+    <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Button
+      <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+        <path d="M5 12h14M12 5l7 7-7 7"></path>
+      </svg>
+    </button>
+  </div>
+</header>   
+    </div>
+	<div class="box row-start-2 row-span-4 col-start-1 col-span-3">
+        
+
+    <div class="overflow-x-auto relative shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6 bg-gray-50 dark:bg-gray-800">
+                    Product name
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Color
+                </th>
+                <th scope="col" class="py-3 px-6 bg-gray-50 dark:bg-gray-800">
+                    Category
+                </th>
+                <th scope="col" class="py-3 px-6">
+                    Price
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Apple MacBook Pro 17"
+                </th>
+                <td class="py-4 px-6">
+                    Sliver
+                </td>
+                <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">
+                    Laptop
+                </td>
+                <td class="py-4 px-6">
+                    $2999
+                </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Microsoft Surface Pro
+                </th>
+                <td class="py-4 px-6">
+                    White
+                </td>
+                <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">
+                    Laptop PC
+                </td>
+                <td class="py-4 px-6">
+                    $1999
+                </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Magic Mouse 2
+                </th>
+                <td class="py-4 px-6">
+                    Black
+                </td>
+                <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">
+                    Accessories
+                </td>
+                <td class="py-4 px-6">
+                    $99
+                </td>
+            </tr>
+            <tr class="border-b border-gray-200 dark:border-gray-700">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Google Pixel Phone
+                </th>
+                <td class="py-4 px-6">
+                    Gray
+                </td>
+                <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">
+                    Phone
+                </td>
+                <td class="py-4 px-6">
+                    $799
+                </td>
+            </tr>
+            <tr>
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap bg-gray-50 dark:text-white dark:bg-gray-800">
+                    Apple Watch 5
+                </th>
+                <td class="py-4 px-6">
+                    Red
+                </td>
+                <td class="py-4 px-6 bg-gray-50 dark:bg-gray-800">
+                    Wearables
+                </td>
+                <td class="py-4 px-6">
+                    $999
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+
+
+
+    </div>
+	<div class="box row-start-2 row-span-2 col-start-4 col-end-5">
+        <Lchart />
+    </div>
+	<div class="box row-start-4 row-span-2 col-end-5">
+        <Lchart />
+    </div>
 	<div class="box row-start-6 row-span-1 col-start-1 col-end-5">5</div>
 </div>
   )
