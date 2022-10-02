0fcd7d50002193671263bbec2a26c92f2ad7947c
Crostons <shubh1608003@gmail.com>
2022-10-02 17:16:06 +0530

    Data cards added

---
+++ src/components/Datacard1.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Datacard1() {
+  return (
+    <div className="w-full h-full">
+        
+<a href="#" class="block p-6 max-w-sm bg-sss rounded-lg border border-gray-700 shadow-md w-full mx-4">
+    <h5 class="mb-2 text-2xl font-thin font-barlow tracking-tight text-spicy">Balance</h5>
+    <p class="font-normal text-gray-700 dark:text-gray-400">Lorem ipsum</p>
+</a>
+
+    </div>
+  )
+}
+
+export default Datacard1
--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import Datacard1 from '../components/Datacard1';
 import Lchart from '../components/Lchart';
 import Navbar from '../components/Navbar';
 
 function Dashboard() {
   return (
-    <div className="grid overflow-hidden grid-cols-6 grid-rows-12 gap-2 w-screen h-screen">
+    <div className="grid overflow-hidden grid-cols-6 grid-rows-12 gap-4  w-screen h-screen">
     <div className="box row-start-1 row-span-1 col-start-1 col-span-6"><Navbar /></div>
-    <div className="box row-start-2 row-span-3 col-start-1 col-span-1">2</div>
-    <div className="box row-start-2 row-span-3 col-start-2 col-span-1">3</div>
+    <div className="box row-start-2 row-span-3 col-start-1 col-span-1"><Datacard1 /></div>
+    <div className="box row-start-2 row-span-3 col-start-2 col-span-1"><Datacard1 /></div>
     <div className="box row-start-2 row-span-3 col-start-3 col-span-2">4</div>
     <div className="box row-start-2 row-span-3 col-start-5 col-span-2">5</div>
     <div className="box row-start-5 row-span-4 col-start-1 col-span-3">6</div>
