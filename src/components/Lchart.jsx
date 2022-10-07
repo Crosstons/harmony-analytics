f07e21fa88cefce2eb13cde5d3d26ca37c96dd57
Crostons <shubh1608003@gmail.com>
2022-10-07 20:01:07 +0530

    Lchart edited

--- src/components/Lchart.jsx
+++ src/components/Lchart.jsx
@@ -1,79 +1,238 @@
 import React from "react";
-import {
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip
-} from "recharts";
-
+import styled from "styled-components";
+import { AreaChart, Area, Tooltip, ResponsiveContainer } from "recharts";
+import { cardStyles } from "./ReusableStyles";
 const data = [
+  { data: 4500 },
   {
-    name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400
+    data: 5000,
   },
   {
-    name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210
+    data: 4700,
   },
   {
-    name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290
+    data: 4400,
   },
   {
-    name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000
+    data: 4800,
   },
   {
-    name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181
+    data: 5300,
   },
   {
-    name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500
+    data: 5800,
   },
   {
-    name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100
-  }
+    data: 6000,
+  },
+  {
+    data: 6300,
+  },
+  {
+    data: 6580,
+  },
+  {
+    data: 6780,
+  },
+  {
+    data: 6680,
+  },
+  {
+    data: 6500,
+  },
+  {
+    data: 6300,
+  },
+  {
+    data: 5900,
+  },
+  {
+    data: 5700,
+  },
+  {
+    data: 5500,
+  },
+  {
+    data: 5300,
+  },
+  {
+    data: 5100,
+  },
+  {
+    data: 5090,
+  },
+  {
+    data: 5300,
+  },
+  {
+    data: 5800,
+  },
+  {
+    data: 6000,
+  },
+  {
+    data: 6300,
+  },
+  {
+    data: 6780,
+  },
+  {
+    data: 6500,
+  },
+  {
+    data: 6300,
+  },
+  {
+    data: 6500,
+  },
+  {
+    data: 6700,
+  },
+  {
+    data: 7000,
+  },
+  {
+    data: 7300,
+  },
+  {
+    data: 7500,
+  },
+  {
+    data: 7700,
+  },
+  {
+    data: 8090,
+  },
+  {
+    data: 8190,
+  },
+  {
+    data: 7990,
+  },
+
+  {
+    data: 7700,
+  },
+  {
+    data: 7500,
+  },
+  {
+    data: 7300,
+  },
+  {
+    data: 7000,
+  },
+  {
+    data: 6700,
+  },
+  {
+    data: 6500,
+  },
+  {
+    data: 6300,
+  },
+  {
+    data: 6500,
+  },
+  {
+    data: 6780,
+  },
+  {
+    data: 6300,
+  },
+  {
+    data: 6000,
+  },
+  {
+    data: 5800,
+  },
+
+  {
+    data: 5490,
+  },
+  {
+    data: 6000,
+  },
+  {
+    data: 8000,
+  },
 ];
-
-
-function Lchart() {
+export default function Earnings() {
   return (
-    <AreaChart
-      width={400}
-      height={350}
-      data={data}
-      margin={{
-        top: 10,
-        right: 30,
-        left: 0,
-        bottom: 0
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis />
-      <Tooltip />
-      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-    </AreaChart>
-  )
+    <Section>
+      <div className="top">
+        <div className="info">
+          <h5>This month earnings</h5>
+          <h1>$682.5</h1>
+          <div className="growth">
+            <span>+2.45%</span>
+          </div>
+        </div>
+      </div>
+      <div className="chart">
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={data}
+            margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
+          >
+            <Tooltip cursor={false} />
+            <Area
+              animationBegin={800}
+              animationDuration={2000}
+              type="monotone"
+              dataKey="data"
+              stroke="#ffc107"
+              fill="#8068233e"
+              strokeWidth={4}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    </Section>
+  );
 }
-
-export default Lchart;
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 20rem;
+  ${cardStyles}
+  padding: 2rem 0 0 0;
+  .top {
+    .info {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.2rem;
+      h1 {
+        font-size: 2rem;
+      }
+      .growth {
+        background-color: #d7e41e1d;
+        padding: 0.5rem;
+        border-radius: 1rem;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          background-color: #ffc107;
+          span {
+            color: black;
+          }
+        }
+        span {
+          color: #ffc107;
+        }
+      }
+    }
+  }
+  .chart {
+    height: 70%;
+    .recharts-default-tooltip {
+      background-color: black !important;
+      border-color: black !important;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+  }
+`;
--- src/screens/Dashboard.jsx
+++ src/screens/Dashboard.jsx
@@ -9,7 +9,7 @@ function Dashboard() {
     <div className="box row-start-1 row-span-1 col-start-1 col-span-6"><Navbar /></div>
     <div className="box row-start-2 row-span-3 col-start-1 col-span-1"><Datacard1 /></div>
     <div className="box row-start-2 row-span-3 col-start-2 col-span-1"><Datacard1 /></div>
-    <div className="box row-start-2 row-span-3 col-start-3 col-span-2">4</div>
+    <div className="box row-start-2 row-span-3 col-start-3 col-span-2"><Lchart /></div>
     <div className="box row-start-2 row-span-3 col-start-5 col-span-2">5</div>
     <div className="box row-start-5 row-span-4 col-start-1 col-span-3">6</div>
     <div className="box row-start-5 row-span-4 col-start-4 col-span-3">7</div>
