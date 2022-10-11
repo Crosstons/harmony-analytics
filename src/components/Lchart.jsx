0891b7c7201b18a38160e2cd4830c1b38585943f
Crostons <shubh1608003@gmail.com>
2022-10-11 20:15:23 +0530

    .....

--- src/components/Lchart.jsx
+++ src/components/Lchart.jsx
@@ -1,71 +1,3 @@
-<<<<<<< HEAD
-import React, { PureComponent } from 'react';
-import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
-
-function Lchart() {
-  return (
-    <ResponsiveContainer width="100%" height="100%">
-        <AreaChart
-          width={200}
-          height={60}
-          data={data}
-          margin={{
-            top: 5,
-            right: 0,
-            left: 0,
-            bottom: 5,
-          }}
-        >
-          <Area type="monotone" dataKey="uv" stroke="#28B78D" fill="#28B78D" />
-        </AreaChart>
-      </ResponsiveContainer>
-  )
-=======
 import React, { PureComponent } from "react";
 import styled from "styled-components";
 import { AreaChart, Area, Tooltip, ResponsiveContainer } from "recharts";
@@ -85,12 +17,13 @@ export default class Earnings extends PureComponent {
 
   render() {
   return (
-        <ResponsiveContainer width="100%" height="100%">
+    <div>
+        <ResponsiveContainer width="75%" height="50%">
           <AreaChart
             width={500}
             height={400}
             data={this.state.data.reverse()}
-            margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
+            margin={{ top: 5, left: 25, right: 5, bottom: 5 }}
           >
             <Tooltip cursor={false} />
             <Area
@@ -104,52 +37,7 @@ export default class Earnings extends PureComponent {
             />
           </AreaChart>
         </ResponsiveContainer>
+     </div>
     );
   }
->>>>>>> d4610f18725d5e63060c9459de71682f9e591bf1
 }
-
-export default Lchart
-const Section = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  min-height: 20rem;
-  ${cardStyles}
-  padding: 2rem 0 0 0;
-  .top {
-    .info {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      gap: 0.2rem;
-      h1 {
-        font-size: 2rem;
-      }
-      .growth {
-        background-color: #d7e41e1d;
-        padding: 0.5rem;
-        border-radius: 1rem;
-        transition: 0.3s ease-in-out;
-        &:hover {
-          background-color: #ffc107;
-          span {
-            color: black;
-          }
-        }
-        span {
-          color: #ffc107;
-        }
-      }
-    }
-  }
-  .chart {
-    height: 70%;
-    .recharts-default-tooltip {
-      background-color: black !important;
-      border-color: black !important;
-    }
-  }
-  @media screen and (min-width: 280px) and (max-width: 1080px) {
-  }
-`;
