1869a0a6f1ef9eba4ccbc00f01e0f387b23c3c8b
Vraj Desai <68881630+vking45@users.noreply.github.com>
2022-10-12 09:37:18 +0530

    .

--- src/components/Lchart.jsx
+++ src/components/Lchart.jsx
@@ -16,13 +16,12 @@ export default class Earnings extends PureComponent {
 
   render() {
   return (
-    <div>
-        <ResponsiveContainer width="75%" height="50%">
+        <ResponsiveContainer width="100%" height="100%">
           <AreaChart
             width={500}
             height={400}
             data={this.state.data.reverse()}
-            margin={{ top: 5, left: 25, right: 5, bottom: 5 }}
+            margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
           >
             <Tooltip cursor={false} />
             <Area
@@ -36,7 +35,51 @@ export default class Earnings extends PureComponent {
             />
           </AreaChart>
         </ResponsiveContainer>
-     </div>
     );
   }
 }
+
+// export default Lchart
+const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 20rem;
+  ${cardStyles}
+  padding: 2rem 0 0 0;
+  .top {
+    .info {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.2rem;
+      h1 {
+        font-size: 2rem;
+      }
+      .growth {
+        background-color: #d7e41e1d;
+        padding: 0.5rem;
+        border-radius: 1rem;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          background-color: #ffc107;
+          span {
+            color: black;
+          }
+        }
+        span {
+          color: #ffc107;
+        }
+      }
+    }
+  }
+  .chart {
+    height: 70%;
+    .recharts-default-tooltip {
+      background-color: black !important;
+      border-color: black !important;
+    }
+  }
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+  }
+`;
